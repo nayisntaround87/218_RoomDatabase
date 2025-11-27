759491983bf19f0bca7ab4385fca5a7e8befd1f4
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-27 14:55:07 +0700

    membuat PenyediaViewModel.kt

---
+++ app/src/main/java/com/example/myroomsatu/view/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.myroomsatu.view.viewmodel.provider
+
+import android.text.Editable
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsatu.repositori.AplikasiSiswa
+import com.example.myroomsatu.view.viewmodel.EntryViewModel
+import com.example.myroomsatu.view.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
