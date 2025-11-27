1806cf4f1491fd3e58495bfba132977de69ab4c6
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-27 14:23:42 +0700

    menambahkan file HomeViewModel.kt

---
+++ app/src/main/java/com/example/myroomsatu/view/viewmodel/HomeViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.myroomsatu.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myroomsatu.repositori.RepositoriSiswa
+import com.example.myroomsatu.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import java.util.concurrent.TimeoutException
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+                                                .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+}
