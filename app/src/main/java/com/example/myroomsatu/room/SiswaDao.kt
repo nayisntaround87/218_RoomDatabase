dd560d5db959ba01d83a4f3657730b31886f581e
nayisntaround87 <naylasalwahayati@gmail.com>
2025-12-04 12:03:09 +0700

    modifikasi siswa dao

--- app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
@@ -13,4 +13,8 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
 }
